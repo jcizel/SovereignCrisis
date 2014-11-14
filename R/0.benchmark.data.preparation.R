80e7fec37f7d24a33b0114be0657af378f17f0d4
jcizel <jcizel@gmail.com>
2014-11-14 16:56:59 +0100

    minor additions

--- R/0.benchmark.data.preparation.R
+++ R/0.benchmark.data.preparation.R
@@ -1,3 +1,23 @@
+.trim <- function(str) gsub("^\\s+|\\s+$", "", str)
+
+.condense <- function(str){
+    str.out<- paste(
+        {
+            .x <- unique(.trim(str))
+            .x[order(.x)]
+        },
+        collapse = "; "
+    )
+    return(str.out)
+}
+
+.toDate <- function(x){
+    x      <- as.factor(x)
+    levels <- levels(x)
+    dates  <- as.Date(levels,format = "%Y-%m-%d")
+    return(dates[x])
+}
+
 .updateISOCodes <- function(){
     ctry <- WorldBankAPI:::getWorldBankCountries()
 
@@ -8,7 +28,8 @@
 
 ## .updateISOCodes()
 
-.lookupISOCode <- function (query) 
+.lookupISOCode <- function (query,
+                            queryType = NULL) 
 {
     .files <- list.files('./inst/extdata')
 
@@ -19,11 +40,23 @@
     }
     
     isotype <- max(nchar(query))
-    
-    if (isotype == 2){
+
+    if (is.null(queryType)){
+        if (isotype == 2){
+            setkeyv(ctry, "iso2Code")
+            out <- ctry[J(query)][['id']]
+        } else {
+            setkeyv(ctry, "id")
+            out <- ctry[J(query)][['iso2Code']]        
+        }
+    } else if (queryType == 'name'){
+        ctry[, country := toupper(name)]
+        setkeyv(ctry, "country")
+        out <- ctry[J(toupper(query))][['id']]                
+    } else if (queryType == 'iso2') {
         setkeyv(ctry, "iso2Code")
         out <- ctry[J(query)][['id']]
-    } else {
+    } else if (queryType == 'iso3') {
         setkeyv(ctry, "id")
         out <- ctry[J(query)][['iso2Code']]        
     }
@@ -31,7 +64,7 @@
     return(out)
 }
 
-## .lookupISOCode(query = c("SVN","AUT"))
+## .lookupISOCode(query = c("SVN","AUT"), queryType = 'iso3')
 
 getBloombergSovCDS <- function(){
 
@@ -72,3 +105,69 @@ getSPRatings <- function(){
 }
 
 ## getSPRatings()
+
+getIMFIFS <- function(){
+    imfifs <- fread(input = "./inst/extdata/IMF/IFS/IMF-IFS_annual.csv",
+                    header = TRUE
+                    )
+
+    lookup <- read.xlsx2(file = './inst/extdata/IMF/IFS/IMF-IFS_variables.xlsx',
+                         sheetIndex = 1 )
+    l <- data.table(lookup)
+
+    o1 <- 
+        imfifs[!is.na(value) &
+                   !is.na(iso3),
+               list(iso3,
+                    date = .toDate(date),
+                    varcode,
+                    value)]
+
+    o2 <-
+        data.table:::dcast.data.table(o1,
+                                      iso3 + date ~ varcode,
+                                      value.var = "value")
+
+    attributes(o2)$colnames <- l
+
+    return(o2)
+}
+
+## imf <- getIMFIFS()
+
+getSovBondSpreads <- function(){
+    spread <- read.xlsx2(file = "./inst/extdata/World Bank/GEM/Sovereign Bond Interest Rate Spreads, basis points over US Treasuries.xlsx",
+                       sheetName = "monthly",
+                       header = TRUE,
+                       check.names = FALSE
+                       )[-1,]
+    spread <- data.table(spread)
+
+    spread[, date := {
+        year = as.numeric(sapply(str_split(obs, "M"), function(x) x[[1]]))
+        month = as.numeric(sapply(str_split(obs, "M"), function(x) x[[2]]))
+        as.Date(paste0(year,"-",month,"-01"), format = "%Y-%m-%d")
+    }]
+
+    spread[, obs := NULL]
+    
+    o <- 
+        data.table:::melt.data.table(spread, na.rm = T,
+                                     id.vars = "date",
+                                     variable.name = "country",
+                                     value.name = "BOND.SPREAD")
+    o[, iso3 := .lookupISOCode(
+                 query = toupper(country),
+                 queryType = 'name')]
+
+    o[, spread := as.numeric(BOND.SPREAD)]
+
+    out <-
+        o[, list(iso3,
+                 date,
+                 spread)]
+    return(out)
+}
+
+## spread <- getSovBondSpreads()
+## spread[iso3=='ARG', qplot(date,spread,geom = 'line')]
